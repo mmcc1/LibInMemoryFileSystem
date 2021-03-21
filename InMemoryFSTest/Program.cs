f050a95acd8e8b6e2a051e4b80708d28c01b22d7
Marx Bitware <info@marxbitware.co.uk>
2021-03-21 16:03:00 +0700

    Add project files.

---
+++ InMemoryFSTest/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using LibInMemoryFileSystem;
+
+namespace InMemoryFSTest
+{
+    class Program
+    {
+        public static string version = "1.0.0";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cloakroom FS " + version);
+            Guid[] guids = new Guid[5];
+            string[] results = new string[5];
+
+            string[] samples = new string[]
+            {
+                "This is test string 1",
+                "This is test string 2",
+                "This is test string 3",
+                "This is test string 4",
+                "This is test string 5"
+            };
+
+
+            Cloakroom cr = new Cloakroom();
+
+            Console.WriteLine("Saving...");
+            for (int i = 0; i < samples.Length; i++)
+            {
+                guids[i] = cr.Save(Encoding.ASCII.GetBytes(samples[i]));
+            }
+
+            Console.WriteLine("Reading...");
+            for (int i = 0; i < results.Length; i++)
+            {
+                results[i] = Encoding.ASCII.GetString(cr.Read(guids[i]));
+                Console.WriteLine(results[i]);
+            }
+
+            Console.WriteLine("Saving backup...");
+            File.WriteAllText("backup.cr", cr.Backup());
+
+            Console.WriteLine("Opening backup...");
+            string bk = File.ReadAllText("backup.cr");
+
+            Console.WriteLine("Restoring backup...");
+            cr.Clear();
+            cr.Restore(bk);
+
+            Console.WriteLine("Reading...");
+            //Read
+            for (int i = 0; i < results.Length; i++)
+            {
+                results[i] = Encoding.ASCII.GetString(cr.Read(guids[i]));
+                Console.WriteLine(results[i]);
+            }
+
+            Console.WriteLine("Finished.  Press ENTER to exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LibInMemoryFileSystem/Cloakroom.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace LibInMemoryFileSystem
+{
+    public class CloakroomFolderEntry
+    {
+        public CloakroomFolderEntry()
+        {
+            Files = new List<CloakroomFileEntry>();
+        }
+
+        public List<CloakroomFileEntry> Files { get; set; }
+    }
+
+    public class CloakroomFileEntry
+    {
+        public CloakroomFileEntry()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public byte[] FileContents { get; set; }
+    }
+
+    public class Cloakroom
+    {
+        private CloakroomFolderEntry root;
+
+        public Cloakroom()
+        {
+            root = new CloakroomFolderEntry();
+        }
+
+        public Guid Save(byte[] data)
+        {
+            CloakroomFileEntry fe = new CloakroomFileEntry() { FileContents = data };
+            root.Files.Add(fe);
+            return fe.Id;
+        }
+
+        public byte[] Read(Guid id)
+        {
+            for (int i = 0; i < root.Files.Count; i++)
+            {
+                if (root.Files[i].Id == id)
+                    return root.Files[i].FileContents;
+            }
+
+            return null;
+        }
+
+        public void Delete(Guid id)
+        {
+            for (int i = 0; i < root.Files.Count; i++)
+            {
+                if (root.Files[i].Id == id)
+                    root.Files.RemoveAt(i);
+            }
+        }
+
+        public void Clear()
+        {
+            root.Files.Clear();
+        }
+
+        public string Backup()
+        {
+            return JsonConvert.SerializeObject(root);
+        }
+
+        public void Restore(string backup)
+        {
+            root = JsonConvert.DeserializeObject<CloakroomFolderEntry>(backup);
+        }
+    }
+}
